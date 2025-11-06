6e373801f8e48c3a024d975be4ca11f18c51edb6
michaelmccamy <michael.mccamy@ufl.edu>
2025-11-06 17:46:43 -0500

    Fixed port issue

--- src/main/java/com/example/taskmanager/config/WebConfig.java
+++ src/main/java/com/example/taskmanager/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowedHeaders("*");
             }
